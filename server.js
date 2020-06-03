62df07b99295f200f3b4e34601e48ce616d7ed6e
Ujwal Kashyap <usualketchup@berkeley.edu>
2020-06-02 21:08:09 -0700

    updated server for heroku deployment

--- server.js
+++ server.js
@@ -65,6 +65,8 @@ app.delete("/api/notes/:id", function (req, res) {
 })
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-})
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+app.listen(port);
